2fe14085cbc57423590f239138ec92e2ae96a8ae
Duron27 <duron27@gmail.com>
2026-09-22 10:22:32 -0400

    maybe?

--- triplets/arm64-android.cmake
+++ triplets/arm64-android.cmake
@@ -8,22 +8,14 @@ else()
 endif()
 
 # ANGLE requires specific handling
-if(${PORT} MATCHES "angle")
+if(${PORT} MATCHES "openal|sdl2|collada|glad|angle")
     set(VCPKG_CRT_LINKAGE dynamic)
     set(VCPKG_LIBRARY_LINKAGE dynamic)
-    set(VCPKG_CMAKE_CONFIGURE_OPTIONS
-            -DANDROID_ABI=arm64-v8a
-            -DBUILD_SHARED_LIBS=ON
-    )
-    set(VCPKG_LINKER_FLAGS "-llog -landroid -lnativewindow")
-elseif(${PORT} MATCHES "openal|sdl2|collada")
-    set(VCPKG_CRT_LINKAGE dynamic)
-    set(VCPKG_LIBRARY_LINKAGE dynamic)
-    set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
+    set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${COMMON_CMAKE_ARGS})
 else()
     set(VCPKG_CRT_LINKAGE static)
     set(VCPKG_LIBRARY_LINKAGE static)
-    set(VCPKG_CMAKE_CONFIGURE_OPTIONS -DANDROID_ABI=arm64-v8a)
+    set(VCPKG_CMAKE_CONFIGURE_OPTIONS ${COMMON_CMAKE_ARGS})
 endif()
 
 set(VCPKG_MAKE_BUILD_TRIPLET "--host=aarch64-linux-android")
